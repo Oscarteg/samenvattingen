fca4ff53baaf411a830de9487788e61a58f151bd
Oscar te Giffel <oscar@tegiffel.com>
2018-08-10 15:00:53 +0200

    Change theme

---
+++ docs/home/components/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import styled from 'react-emotion'
+
+const Wrapper = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 0;
+`
+
+export const Footer = () => <Wrapper>Oscar te Giffel</Wrapper>
